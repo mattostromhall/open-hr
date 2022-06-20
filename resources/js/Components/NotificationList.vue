722d1a5e95986fe41d14554139b751701b3ed550
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-06-20 14:39:04 +0100

    fixed issue with mark as read/unread closing slide over

--- resources/js/Components/NotificationList.vue
+++ resources/js/Components/NotificationList.vue
@@ -22,44 +22,50 @@ const {hideSlideOver} = useNotificationsSlideOver()
             v-for="notification in notifications"
             :key="notification.body"
         >
-            <Link
+            <div
                 v-if="notification.link"
-                :href="notification.link"
-                class="block hover:bg-gray-50"
-                @click="hideSlideOver"
+                class="group"
             >
-                <div class="flex items-center p-4 sm:px-6">
-                    <div>
-                        <h3
-                            v-show="notification.title"
-                            class="text-sm font-semibold text-gray-800"
-                        >
-                            {{ notification.title }}
-                        </h3>
-                        <p class="mt-1 text-sm text-gray-600">
-                            {{ notification.body }}
-                            <Link
-                                :href="`/notifications/${notification.id}/read`"
-                                :data="{read: ! notification.read}"
-                                method="post"
-                                as="button"
-                                type="button"
-                                class="block mt-1 text-sm text-indigo-500"
-                                preserve-scroll
-                                preserve-state
+                <Link
+                    :href="notification.link"
+                    class="block group-hover:bg-gray-50"
+                    @click="hideSlideOver"
+                >
+                    <div class="flex items-center px-4 pt-4 space-x-2 sm:px-6">
+                        <div>
+                            <h3
+                                v-show="notification.title"
+                                class="text-sm font-semibold text-gray-800"
                             >
-                                {{ notification.read ? 'Mark as unread' : 'Mark as read' }}
-                            </Link>
-                        </p>
+                                {{ notification.title }}
+                            </h3>
+                            <p class="mt-1 text-sm text-gray-600">
+                                {{ notification.body }}
+                            </p>
+                        </div>
+                        <div>
+                            <ChevronRightIcon class="w-5 h-5 text-gray-400" />
+                        </div>
                     </div>
-                    <div>
-                        <ChevronRightIcon class="w-5 h-5 text-gray-400" />
-                    </div>
-                </div>
-            </Link>
+                </Link>
+                <p class="px-4 pt-1 pb-4 text-sm text-gray-600 group-hover:bg-gray-50 sm:px-6">
+                    <Link
+                        :href="`/notifications/${notification.id}/read`"
+                        :data="{read: ! notification.read}"
+                        method="post"
+                        as="button"
+                        type="button"
+                        class="block mt-1 text-sm text-indigo-500"
+                        preserve-scroll
+                        preserve-state
+                    >
+                        {{ notification.read ? 'Mark as unread' : 'Mark as read' }}
+                    </Link>
+                </p>
+            </div>
             <div
                 v-else
-                class="p-4"
+                class="p-4 sm:px-6"
             >
                 <h3
                     v-show="notification.title"
@@ -67,7 +73,7 @@ const {hideSlideOver} = useNotificationsSlideOver()
                 >
                     {{ notification.title }}
                 </h3>
-                <p class="mt-1 text-sm text-gray-600 sm:px-6">
+                <p class="mt-1 text-sm text-gray-600">
                     {{ notification.body }}
                     <Link
                         :href="`/notifications/${notification.id}/read`"
