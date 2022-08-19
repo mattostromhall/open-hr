8fb54aa901b41173ac7c418a92b143c847a1c652
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-08-19 17:26:18 +0100

    editor input wip

---
+++ resources/js/Components/Controls/EditorInput.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+    extensions: [
+        StarterKit,
+    ],
+    editorProps: {
+        attributes: {
+            class: 'w-full prose prose-sm sm:prose lg:prose-lg xl:prose-2xl m-5 focus:outline-none bg-white rounded-md shadow-md p-4',
+        },
+    },
+    content: `
+    <h2>
+      Hi there,
+    </h2>
+    <p>
+      this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+    </p>
+    <ul>
+      <li>
+        That’s a bullet list with one …
+      </li>
+      <li>
+        … or two list items.
+      </li>
+    </ul>
+    <p>
+      Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+    </p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+    <p>
+      I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+    </p>
+    <blockquote>
+      Wow, that’s amazing. Good work, boy! 👏
+      <br />
+      — Mom
+    </blockquote>
+  `,
+})
+</script>
+
+<template>
+    <EditorContent :editor="editor" />
+</template>
