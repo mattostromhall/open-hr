8df9dc9bfb94c15a41c809f7c11120bf890a12b6
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-09-17 11:55:25 +0100

    made notifications prunable to avoid large numbers of notifications being loaded on dashboard requests

--- src/Domain/Notifications/Models/Notification.php
+++ src/Domain/Notifications/Models/Notification.php
@@ -2,8 +2,10 @@
 
 namespace Domain\Notifications\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Support\Concerns\Unguarded;
 
@@ -11,6 +13,7 @@ class Notification extends Model
 {
     use HasFactory;
     use Unguarded;
+    use Prunable;
 
     protected $casts = [
         'read' => 'boolean'
@@ -19,5 +22,13 @@ class Notification extends Model
     public function notifiable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function prunable(): Builder
+    {
+        return self::query()
+            ->where('read', true)
+            ->whereNot('notifiable_type', 'organisation')
+            ->where('created_at', '<=', now()->subMonth());
     }
 }
