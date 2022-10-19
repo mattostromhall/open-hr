882954a86f178aa85d4822bec3315b8f257bb0e4
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-10-19 09:42:21 +0100

    UpdateActiveControllerTest wip

---
+++ tests/Feature/Auth/UpdateActiveControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Domain\Organisation\Models\Organisation;
+use Domain\People\Models\Person;
+use Silber\Bouncer\BouncerFacade as Bouncer;
+
+beforeEach(function () {
+    Organisation::factory()->create();
+    $this->person = Person::factory()->create();
+    $this->actingAs($this->person->user);
+});
+
+it('deactivates the user', function () {
+    $this->patch(route('user.update.active', [
+        'user' => $this->person->user
+    ]), [
+        'active' => false
+    ])
+        ->assertStatus(302)
+        ->assertSessionHas('flash.success', 'Deactivated!');
+});
+
+it('activates the user', function () {
+    $this->patch(route('user.update.active', [
+        'user' => $this->person->user
+    ]), [
+        'active' => true
+    ])
+        ->assertStatus(302)
+        ->assertSessionHas('flash.success', 'Activated!');
+});
