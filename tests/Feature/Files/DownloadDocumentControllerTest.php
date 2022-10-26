e9b7e01ec64535d6d4a9cca965e060128aa061a5
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-10-26 10:08:21 +0100

    DownloadDocumentControllerTest implemented

---
+++ tests/Feature/Files/DownloadDocumentControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Domain\Files\Enums\DocumentableType;
+use Domain\Organisation\Models\Organisation;
+use Domain\People\Models\Person;
+use Illuminate\Http\UploadedFile;
+
+beforeEach(function () {
+    Organisation::factory()->create();
+    $this->person = Person::factory()->create();
+    $this->actingAs($this->person->user);
+});
+
+it('downloads the document for the path provided', function () {
+    $this->post(route('document.store'), [
+        'path' => '/documents/test',
+        'documents' => [UploadedFile::fake()->create('document.pdf', 10)],
+        'documentable_id' => $this->person->id,
+        'documentable_type' => DocumentableType::PERSON->value
+    ]);
+
+    $response = $this->get(
+        route('document.download', ['path' => 'documents/test/document.pdf'])
+    );
+
+    $response->assertStatus(200);
+});
+
+it('returns a 404 if the document does not exist', function () {
+    $response = $this->get(
+        route('document.download', ['path' => '123456789'])
+    );
+
+    $response->assertStatus(404);
+});
