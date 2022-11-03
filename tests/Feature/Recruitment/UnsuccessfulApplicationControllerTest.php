533dcea0057619eaf06fab4ee3ee8b430febe002
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2022-11-03 08:59:40 +0000

    UnsuccessfulApplicationControllerTest implemented

---
+++ tests/Feature/Recruitment/UnsuccessfulApplicationControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Domain\Organisation\Models\Organisation;
+use Domain\People\Models\Person;
+use Domain\Recruitment\Enums\ApplicationStatus;
+use Domain\Recruitment\Models\Application;
+
+beforeEach(function () {
+    Organisation::factory()->create();
+    $this->person = Person::factory()->create();
+    $this->actingAs($this->person->user);
+});
+
+it('marks an application as unsuccessful', function () {
+    $application = Application::factory()->create();
+
+    $response = $this->post(route('application.unsuccessful', ['application' => $application]));
+
+    $response
+        ->assertStatus(302)
+        ->assertSessionHas('flash.success', 'Application marked as unsuccessful!');
+});
