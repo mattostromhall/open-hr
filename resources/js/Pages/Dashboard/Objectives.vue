1c1dd84eb64762ab077181255df16c5dba732b39
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2023-01-13 20:31:56 +0000

    set objectives tab as active in dashboard objectives

--- resources/js/Pages/Dashboard/Objectives.vue
+++ resources/js/Pages/Dashboard/Objectives.vue
@@ -59,7 +59,7 @@ defineProps<{
                 </div>
                 <div class="mt-6">
                     <Link
-                        href="/performance"
+                        href="/performance?active=current"
                         class="flex w-full items-center justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
                     >
                         View all
